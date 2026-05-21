7be962c94e77aed9a3ea485c1abb04e30d52d835
Joachim Metz <joachim.metz@gmail.com>
2026-05-21 10:25:59 +0200

    Worked on script to generate specimens

--- generate-specimens-bsd.sh
+++ generate-specimens-bsd.sh
@@ -3,8 +3,8 @@
 # Script to generate BSD disklabel test files
 # Requires BSD with dd and bsdlabel
 
-EXIT_SUCCESS=0;
-EXIT_FAILURE=1;
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
 
 # Checks the availability of a binary and exits if not available.
 #
@@ -13,42 +13,41 @@ EXIT_FAILURE=1;
 #
 assert_availability_binary()
 {
-	local BINARY=$1;
+	local BINARY=$1
 
-	which ${BINARY} > /dev/null 2>&1;
-	if test $? -ne ${EXIT_SUCCESS};
+	which ${BINARY} > /dev/null 2>&1
+	if test $? -ne ${EXIT_SUCCESS}
 	then
-		echo "Missing binary: ${BINARY}";
-		echo "";
+		echo "Missing binary: ${BINARY}"
+		echo ""
 
-		exit ${EXIT_FAILURE};
+		exit ${EXIT_FAILURE}
 	fi
 }
 
-assert_availability_binary bsdlabel;
-assert_availability_binary dd;
+assert_availability_binary bsdlabel
+assert_availability_binary dd
 
-SPECIMENS_PATH="specimens/bsdlabel";
+SPECIMENS_PATH="specimens/bsdlabel"
 
-if test -d ${SPECIMENS_PATH};
+if test -d ${SPECIMENS_PATH}
 then
-	echo "Specimens directory: ${SPECIMENS_PATH} already exists.";
+	echo "Specimens directory: ${SPECIMENS_PATH} already exists."
 
-	exit ${EXIT_FAILURE};
+	exit ${EXIT_FAILURE}
 fi
 
-mkdir -p ${SPECIMENS_PATH};
+mkdir -p ${SPECIMENS_PATH}
 
-set -e;
+set -e
 
-IMAGE_SIZE=$(( 4 * 1024 * 1024 ));
-SECTOR_SIZE=512;
+IMAGE_SIZE=$(( 4 * 1024 * 1024 ))
+SECTOR_SIZE=512
 
-IMAGE_FILE="${SPECIMENS_PATH}/disklabel.raw";
+IMAGE_FILE="${SPECIMENS_PATH}/disklabel.raw"
 
-dd if=/dev/zero of=${IMAGE_FILE} bs=${SECTOR_SIZE} count=${IMAGE_SIZE} 2> /dev/null;
+dd if=/dev/zero of=${IMAGE_FILE} bs=${SECTOR_SIZE} count=${IMAGE_SIZE} 2> /dev/null
 
-bsdlabel -w -f ${IMAGE_FILE};
+bsdlabel -w -f ${IMAGE_FILE}
 
-exit ${EXIT_SUCCESS};
-
+exit ${EXIT_SUCCESS}
